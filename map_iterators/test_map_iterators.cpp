d95681f3dd88b6d66fa18178c17ad69d38a36ae1
Thorsten Raasch <raasch@mathematik.uni-siegen.de>
2023-03-09 14:33:12 +0100

    tested custom container class, given via third template parameter

--- map_iterators/test_map_iterators.cpp
+++ map_iterators/test_map_iterators.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <algorithm>
 
 /*
  As one of the core ingredients of the AMSTeL library, we will use a C++
- implementation of infinite real-valued sequences over a countable index set
+ implementation of infinite scalar-valued sequences over a countable index set
  with only finitely many nontrivial entries. Mathematically speaking, a minimal
  implementation provides something like c_0, the space of all finitely
- supported, real-valued sequences over the natural numbers.
+ supported, real- or complex-valued sequences over the natural numbers.
  
  In this design test program, we will work out a minimal C++ implementation of
  such objects, which will be called InfiniteVector. In order to use as much
@@ -15,11 +16,12 @@
  the class InfiniteVector from std::map and endow it with some standard linear
  algebra functionality (BLAS level 1 and 2, essentially). Using std::map
  as a protected base class leaves open the option to exchange the internal
- implementation of InfiniteVector, e.g., by a hashed map.
+ implementation of InfiniteVector, e.g., by a hashed map like std::unordered_map
+ or even a custom container class (just change the template parameter CONTAINER)
  
  Main design goals and problems:
- 1) Read and write access should be of O(N) complexity, N being the number of
-    nonzero entries. This is ensured by the standard implementation of
+ 1) Read and write access should be at most of O(N) complexity, N being the
+    number of nonzero entries. This is ensured by the standard implementation of
     std::map as a red/black tree. We are aware of the fact that read access via
     std::map::operator [] alone may insert new, unwanted entries into the
     infinite vector. Therefore, since we want to use the notation [] (and not
@@ -35,52 +37,54 @@
     write this design test program). This requires the existence of suitable
     forward iterator classes by which one can walk through the nontrivial
     entries.
+ 3) We enable the user to exchange the base container class by a third
+    template argument CONTAINER, defaulting to std::map<I,C>.
  
- Thorsten Raasch, November 2018
+ Thorsten Raasch, November 2018 and March 2023
  */
 
 using std::cout;
 using std::endl;
 
 // forward declaration of InfiniteVector iterators
-template <class C, class I> class InfiniteVectorConstIterator;
+template <class C, class I, class CONTAINER> class InfiniteVectorConstIterator;
 
-template <class C, class I=int>
+template <class C, class I=int, class CONTAINER=std::map<I,C> >
 class InfiniteVector
-  : protected std::map<I,C>
+  : protected CONTAINER
 {
 public:
-  friend class InfiniteVectorConstIterator<C,I>;
-  typedef InfiniteVectorConstIterator<C,I> const_iterator;
+  friend class InfiniteVectorConstIterator<C,I,CONTAINER>;
+  typedef InfiniteVectorConstIterator<C,I,CONTAINER> const_iterator;
   
-  typedef typename std::map<I,C>::value_type value_type;
+  typedef typename CONTAINER::value_type value_type;
   
   InfiniteVector()
-  : std::map<I,C> ()
+  : CONTAINER()
   {
   }
   
-  InfiniteVector(const std::map<I,C>& source)
-  : std::map<I,C> (source)
+  InfiniteVector(const CONTAINER& source)
+  : CONTAINER(source)
   {
   }
   
   const_iterator begin() const
   {
-    return const_iterator(*this, std::map<I,C>::begin());
+    return const_iterator(*this, CONTAINER::begin());
   }
   
   const_iterator end() const
   {
-    return const_iterator(*this, std::map<I,C>::end());
+    return const_iterator(*this, CONTAINER::end());
   };
   
   size_t size() const
   {
-    return std::map<I,C>::size();
+    return CONTAINER::size();
   };
   
-  bool operator == (const InfiniteVector<C,I>& v) const
+  bool operator == (const InfiniteVector<C,I,CONTAINER>& v) const
   {
 #if 1
     // this implementation is desirable (using code from <algorithm>),
@@ -100,8 +104,8 @@ public:
   }
 };
 
-template<class C, class I>
-std::ostream& operator << (std::ostream& os, const InfiniteVector<C,I>& v)
+template<class C, class I, class CONTAINER>
+std::ostream& operator << (std::ostream& os, const InfiniteVector<C,I,CONTAINER>& v)
 {
   if (v.begin() == v.end())
   {
@@ -109,7 +113,7 @@ std::ostream& operator << (std::ostream& os, const InfiniteVector<C,I>& v)
   }
   else
   {
-    for (typename InfiniteVector<C,I>::const_iterator it(v.begin());
+    for (typename InfiniteVector<C,I,CONTAINER>::const_iterator it(v.begin());
          it != v.end(); ++it)
     {
       os << it.index() << ": " << it.value() << std::endl;
@@ -119,88 +123,88 @@ std::ostream& operator << (std::ostream& os, const InfiniteVector<C,I>& v)
   return os;
 }
 
-template <class C, class I>
+template <class C, class I, class CONTAINER>
 class InfiniteVectorConstIterator
-: protected std::map<I,C>::const_iterator
+: protected CONTAINER::const_iterator
 {
 public:
-  typedef typename std::map<I,C>::const_iterator::iterator_category iterator_category;
-  typedef typename std::map<I,C>::const_iterator::difference_type difference_type;
-  typedef typename std::map<I,C>::const_iterator::value_type value_type;
-  typedef typename std::map<I,C>::const_iterator::pointer pointer;
-  typedef typename std::map<I,C>::const_iterator::reference reference;
+  typedef typename CONTAINER::const_iterator::iterator_category iterator_category;
+  typedef typename CONTAINER::const_iterator::difference_type difference_type;
+  typedef typename CONTAINER::const_iterator::value_type value_type;
+  typedef typename CONTAINER::const_iterator::pointer pointer;
+  typedef typename CONTAINER::const_iterator::reference reference;
   
 private:
-  const InfiniteVector<C,I>& _container;
+  const InfiniteVector<C,I,CONTAINER>& _container;
   
 public:
-  InfiniteVectorConstIterator(const InfiniteVector<C,I>& container,
-                              typename std::map<I,C>::const_iterator state)
-  : std::map<I,C>::const_iterator(state), _container(container)
+  InfiniteVectorConstIterator(const InfiniteVector<C,I,CONTAINER>& container,
+                              typename CONTAINER::const_iterator state)
+  : CONTAINER::const_iterator(state), _container(container)
   {
   }
 
-  bool operator == (const InfiniteVectorConstIterator<C,I>& it) const
+  bool operator == (const InfiniteVectorConstIterator<C,I,CONTAINER>& it) const
   {
-    return (static_cast<typename std::map<I,C>::const_iterator>(*this)
-            == static_cast<typename std::map<I,C>::const_iterator>(it));
+    return (static_cast<typename CONTAINER::const_iterator>(*this)
+            == static_cast<typename CONTAINER::const_iterator>(it));
   }
   
-  bool operator != (const InfiniteVectorConstIterator<C,I>& it) const
+  bool operator != (const InfiniteVectorConstIterator<C,I,CONTAINER>& it) const
   {
     return !(*this == it);
   }
   
-  InfiniteVectorConstIterator<C,I>& operator ++ ()
+  InfiniteVectorConstIterator<C,I,CONTAINER>& operator ++ ()
   {
-    std::map<I,C>::const_iterator::operator ++ ();
+    CONTAINER::const_iterator::operator ++ ();
     return *this;
   }
  
-  InfiniteVectorConstIterator<C,I> operator ++ (int step)
+  InfiniteVectorConstIterator<C,I,CONTAINER> operator ++ (int step)
   {
-    InfiniteVectorConstIterator<C,I> r(*this);
-    std::map<I,C>::const_iterator::operator ++ (step);
+    InfiniteVectorConstIterator<C,I,CONTAINER> r(*this);
+    CONTAINER::const_iterator::operator ++ (step);
     return r;
   }
   
-  InfiniteVectorConstIterator<C,I>& operator -- ()
+  InfiniteVectorConstIterator<C,I,CONTAINER>& operator -- ()
   {
-    std::map<I,C>::const_iterator::operator -- ();
+    CONTAINER::const_iterator::operator -- ();
     return *this;
   }
   
-  InfiniteVectorConstIterator<C,I> operator -- (int step)
+  InfiniteVectorConstIterator<C,I,CONTAINER> operator -- (int step)
   {
-    InfiniteVectorConstIterator<C,I> r(*this);
-    std::map<I,C>::const_iterator::operator -- (step);
+    InfiniteVectorConstIterator<C,I,CONTAINER> r(*this);
+    CONTAINER::const_iterator::operator -- (step);
     return r;
   }
   
   const I& index() const
   {
-    return (std::map<I,C>::const_iterator::operator * ()).first;
+    return (CONTAINER::const_iterator::operator * ()).first;
   }
 
   const C& value() const
   {
-    return (std::map<I,C>::const_iterator::operator * ()).second;
+    return (CONTAINER::const_iterator::operator * ()).second;
   }
 
   const reference operator * () const
   {
-    return std::map<I,C>::const_iterator::operator * ();
+    return CONTAINER::const_iterator::operator * ();
   }
   
   const pointer operator -> () const
   {
-    return std::map<I,C>::const_iterator::operator -> ();
+    return CONTAINER::const_iterator::operator -> ();
   }
 };
 
-template <class C, class I=int>
+template <class C, class I=int, class CONTAINER=std::map<I,C> >
 class AnotherInfiniteVector
-: public std::map<I,C>
+: public CONTAINER
 {
 public:
   // Note that the class std::map already has an operator ==, which we
@@ -211,9 +215,9 @@ public:
   }
 };
 
-template <class C, class I=int>
+template <class C, class I=int, class CONTAINER=std::map<I,C> >
 class YetAnotherInfiniteVector
-: protected std::map<I,C>
+: protected CONTAINER
 {
 public:
   // Note that the class std::map already has an operator ==, which we
@@ -242,11 +246,14 @@ private:
 
 int main()
 {
-  InfiniteVector<double,int> v;
+  InfiniteVector<double,int> v; // default CONTAINER=std::map<I,C>
+  InfiniteVector<double,int,std::unordered_map<int,double> > z; // custom choice of CONTAINER
   
   // test default constructor and operator <<
   cout << "- a zero vector v:" << endl
     << v;
+  cout << "- a zero vector z, using std::unordered_map as CONTAINER:" << endl
+    << z;
   
   // test constructor from std::map
   std::map<int,double> wmap;
@@ -256,8 +263,16 @@ int main()
   cout << "- a vector w created via std::map :" << endl
     << w;
 
+  // test constructor from custom CONTAINER, e.g., from std::unordered_map
+  std::unordered_map<int,double> umap;
+  umap[42]=23.0;
+  umap[123]=23.0;
+  InfiniteVector<double,int,std::unordered_map<int,double> > u(umap);
+  cout << "- a vector u created via std::unordered_map :" << endl
+    << u;
+
   // test operator == on AnotherInfiniteVector
-  AnotherInfiniteVector<double,int> a,b;
+  AnotherInfiniteVector<double,int> a,b; // container class is public
   a[1]=2.5;
   b[2]=2.5;
   cout << "- are the vectors a and b equal?" << endl;
@@ -267,7 +282,7 @@ int main()
     cout << "  ... no!" << endl;
 
   // test operator == on YetAnotherInfiniteVector
-  YetAnotherInfiniteVector<double,int> ay,by;
+  YetAnotherInfiniteVector<double,int> ay,by; // container class is protected
   cout << "- are the vectors ay and by equal?" << endl;
   if (ay==by)
     cout << "  ... yes!" << endl;
@@ -283,13 +298,18 @@ int main()
   
   // test std::count_if() algorithm for std::map
   const double number=23.0;
-  cout << " wmap contains "
+  cout << "- wmap contains "
   << std::count_if(wmap.begin(), wmap.end(), second_equal_to<std::map<int,double>::value_type>(number))
   << " times the number " << number << endl;
   
   // test std::count_if() algorithm for InfiniteVector
   cout << "- w contains "
   << std::count_if(w.begin(), w.end(), second_equal_to<InfiniteVector<double,int>::value_type>(number))
+  << " times the number " << number << endl;
+
+  // test std::count_if() algorithm for InfiniteVector with custom container
+  cout << "- u contains "
+  << std::count_if(u.begin(), u.end(), second_equal_to<InfiniteVector<double,int,std::unordered_map<int,double> >::value_type>(number))
   << " times the number " << number << endl;
 
   return 0;
